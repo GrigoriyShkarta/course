9e2dac24083e9c24901097f06199f417bf95b091
Grigoriy <grishkoshkarta1@gmail.com>
2022-12-26 18:57:10 +0200

    first day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang';
+  posts: Post[] = [
+    {title: '1 title', text: '1 text', id: 1},
+    {title: '2 title', text: '2 text', id: 2}
+  ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+  }
 }
